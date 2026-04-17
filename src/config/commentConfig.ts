94b8288ac56936384292a4f076bff089e18696a9
cairangxianmu <cairangxianmu@163.com>
2026-04-18 01:24:41 +0800

    修改评论

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -16,7 +16,7 @@ export const commentConfig: CommentConfig = {
 	//waline评论系统配置
 	waline: {
 		// waline 后端服务地址
-		serverURL: "https://waline-fh5cq3mhe-cairangxianmu-5763s-projects.vercel.app",
+		serverURL: "https://waline-blue-pi.vercel.app",
 		// 设置 Waline 评论系统语言
 		lang: "zh-CN",
 		// 设置 Waline 评论系统表情地址
